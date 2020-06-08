9e367929e98730f5ce686f07abca02fbfd80d055
asahinadev <dev.account.01@mail.mirageworld.jp>
2020-06-08 21:02:53 +0900

    パッケージ変更

--- src/main/java/jp/mirageworld/spring/controller/IndexController.java
+++ src/main/java/jp/mirageworld/spring/controller/IndexController.java
@@ -1,4 +1,4 @@
-package jp.mirageworld.spring;
+package jp.mirageworld.spring.controller ;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
