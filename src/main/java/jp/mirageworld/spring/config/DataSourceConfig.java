8f2a44325acdca97859a839e11f2be86efd5b30c
朝日奈ありす <59528169+asahinadev@users.noreply.github.com>
2020-07-08 19:48:47 +0900

    context.xml は管理から外す。
    設定してない場合は メモリから

--- src/main/java/jp/mirageworld/spring/config/DataSourceConfig.java
+++ src/main/java/jp/mirageworld/spring/config/DataSourceConfig.java
@@ -4,14 +4,24 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.lookup.DataSourceLookupFailureException;
 import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Configuration
 public class DataSourceConfig {
 
 	@Bean
 	public DataSource dataSource() {
-		return new JndiDataSourceLookup().getDataSource("jdbc/demo");
+		try {
+			return new JndiDataSourceLookup().getDataSource("jdbc/spring-webmvc-demo");
+		} catch (DataSourceLookupFailureException e) {
+			log.warn(e.getMessage(), e);
+			return new DriverManagerDataSource("jdbc:h2::mem:test", "sa", "");
+		}
 	}
 
 }
